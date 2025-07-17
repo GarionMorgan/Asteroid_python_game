d955d142ad8753aa85fbf378ac0c12795b6665a8
github.com/GarionMorgan <garmorgan14@yahoo.com>
2025-07-17 15:22:56 -0500

    Created main.py file and constants.py file

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame
+from constants import *
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    #creating screen
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    game_loop = True
+
+    while(game_loop):
+        #exits out of the game when user quits
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        #background color
+        screen.fill((0,0,0))
+        #updates the screen
+        pygame.display.flip()
+
+
+
+if __name__ == "__main__":
+    main()
