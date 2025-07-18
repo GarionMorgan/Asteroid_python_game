d86e0118a35533bf8d3ae4b3ca3ef24825514aa5
github.com/GarionMorgan <garmorgan14@yahoo.com>
2025-07-18 13:26:39 -0500

    Added asteroid.py and asteroidfield.py

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    #initiate pygame
+    pygame.init()
     #creating screen
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     #pygame time clock to help keep track of time
     clock = pygame.time.Clock()
+
+    #adding groups to help reduce CPU usage and keep track of objects
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    #container for asteroids
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
     #initialize player
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
     dt = 0
@@ -23,14 +40,16 @@ def main():
                 return
             
         #updates the player
-        player.update(dt)
+        updatable.update(dt)
         #background color
         screen.fill((0,0,0))
         #draw the player
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
+
         #updates the screen
         pygame.display.flip()
-        #updates every 60 seconds
+        #limit the framework to 60 FPS
         dt = clock.tick(60) / 1000
 
 
